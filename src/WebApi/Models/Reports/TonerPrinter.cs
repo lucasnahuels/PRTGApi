dc6ae73ae35d28740c566c3cf6d69342bb631001
softvision.lucas.stirpe2 <lucas.stirpe@softvision.com>
2020-07-21 19:37:52 -0300

    changing name tonner to toner

---
+++ src/WebApi/Models/Reports/TonerPrinter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WebApi.Models.Reports
+{
+    public class TonerPrinter : BaseModel
+    {
+        public long PrinterId { get; set; }
+        public DateTime Date { get; set; }
+        
+//-Cyan | Porcentaje
+//-Yellow | Porcentaje
+//-Magenta | Porcentaje
+//-Negro | Porcentaje
+
+    }
+}
