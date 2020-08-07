0012c61288bb5239d39059a9b0202e9aa67ec1fa
Matias Grieben <matigrieben@gmail.com>
2020-08-07 00:54:37 -0300

    Updated employee to owner reference

---
+++ src/ApplicationCore/EntityFramework/Migrations/20200807035401_InitialMigration2.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ApplicationCore.EntityFramework.Migrations
+{
+    public partial class InitialMigration2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Owners_OwnerId",
+                table: "Employees");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "OwnerId",
+                table: "Employees",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Owners_OwnerId",
+                table: "Employees",
+                column: "OwnerId",
+                principalTable: "Owners",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Owners_OwnerId",
+                table: "Employees");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "OwnerId",
+                table: "Employees",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Owners_OwnerId",
+                table: "Employees",
+                column: "OwnerId",
+                principalTable: "Owners",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
--- src/ApplicationCore/Models/Employee.cs
+++ src/ApplicationCore/Models/Employee.cs
@@ -6,6 +6,7 @@ namespace ApplicationCore.Models
     {
         public string Name { get; set; }
         public string Email { get; set; }
+        public long OwnerId { get; set; }
         public Owner Owner { get; set; }
         public ICollection<ContractEmployee> ContractEmployees { get; set; }
     }
